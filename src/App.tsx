6e318e069c34c522b17fb85cee885c4859bd92ab
i358 <retro98team@gmail.com>
2023-08-05 04:46:23 +0300

    update socket: listen message event

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import "./styles/App.scss";
 import "./styles/tailwind.css";
 import { socket } from './utils/socket'
 const App = () => {
-    let [isConnected, setConnected] = useState(socket.connected)
+    const [isConnected, setConnected] = useState(socket.connected)
+    const [ getTest, setTest ] = useState("Not any messages received.")
     useEffect(() => {
         const established = () => {  
             setConnected(socket.connected)
@@ -11,8 +12,11 @@ const App = () => {
         const disconnected = () => {
             setConnected(socket.connected)
         }
-
+        const messageOn = (message:any) => {
+            setTest(message.content || "unknown message")
+        }
         socket.on("connect", established)
+        socket.on("message", messageOn)
         socket.on("disconnect", disconnected)
     }, [])  
 
@@ -20,6 +24,7 @@ const App = () => {
         <>
             <div className="grid text-[white] text-[45px] font-[600] place-items-center h-[100%]">
                 Socket: {isConnected ? "Connected" : "Disconnected"}
+                Last Message: {getTest}
             </div>
         </>
     )
