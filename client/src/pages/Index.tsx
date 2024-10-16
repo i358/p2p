cd939f137e54bf8aea627d490240c3fdf733c55e
i358 <retro98team@gmail.com>
2024-10-16 21:29:58 +0300

    .

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -12,8 +12,8 @@ return (
     <div className="flex flex-1 flex-col justify-center items-center h-[100%] gap-[4em]" style={{backdropFilter:"blur(2px)"}}>
         
    <div id="text" className="flex items-center flex-col">
-   <p className="text-[9em] font-[900] ">OUTDATED</p>
-   <p className="font-[500] text-[1.7em] text-[#d7d7d7]">This project is outdated. There's nothing to see.</p>
+   <p className="text-[9em] font-[900] ">DEPRECATED</p>
+   <p className="font-[500] text-[1.7em] text-[#d7d7d7]">This project is deprecated. There's nothing to see.</p>
    </div>
    <div className="font-[600] hover:cursor-pointer text-[3em] flex flex-row items-center justify-center gap-[1em] rounded-[15px] p-[15px_30px]" style={{background:"linear-gradient(#171921, #0b0a11)"}}>
     
