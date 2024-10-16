f87979dc2cd013e2720f2a2a7df8d4f73fa07323
i358 <retro98team@gmail.com>
2024-10-16 21:44:07 +0300

    .

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -12,10 +12,10 @@ return (
     <div className="flex flex-1 flex-col justify-center items-center h-[100%] gap-[4em]" style={{backdropFilter:"blur(2px)"}}>
         
    <div id="text" className="flex items-center flex-col">
-   <p className="text-[7.5vw] font-[900] ">DEPRECATED</p>
-   <p className="font-[500] text-[1.4161vw] text-[#d7d7d7]">This project is deprecated. There's nothing to see.</p>
+   <p className="text-[9em] font-[900] ">DEPRECATED</p>
+   <p className="font-[500] text-[1.7em] text-[#d7d7d7]">This project is deprecated. There's nothing to see.</p>
    </div>
-   <div className="font-[600] hover:cursor-pointer text-[2.5vw] flex flex-row items-center justify-center gap-[1em] rounded-[15px] p-[15px_30px]" style={{background:"linear-gradient(#171921, #0b0a11)"}}>
+   <div className="font-[600] hover:cursor-pointer text-[3em] flex flex-row items-center justify-center gap-[1em] rounded-[15px] p-[15px_30px]" style={{background:"linear-gradient(#171921, #0b0a11)"}}>
     
    <i className="fa-brands fa-instagram" onClick={()=>redirect("https://instagram.com/allmylifeisoverthinking")} />
    <i className="fa-brands fa-discord"  onClick={()=>redirect("https://discord.com/")}/>
